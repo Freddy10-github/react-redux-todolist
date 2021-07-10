34f5e160eb95903faf030bca0d2bc2dda3b4bef9
Freddy <a0983330053@gmail.com>
2021-07-10 12:43:14 +0800

    new ver

--- src/redux/contants/action-types.js
+++
@@ -1,4 +0,0 @@
-export const ActionTypes = {
-  Add_TODO:"Add_TODO",
-  DLETE_TODO:"DLETE_TODO"
-}
--- src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux"
-import {taskReducer} from "./reducers"
-
-const reducers = combineReducers({
-  addTask:taskReducer,
-})
-
-export default reducers
